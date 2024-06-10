32e68b4e71c655e9fbb33e07856645f8d1e0309b
iamdarwinsai <darushyam143@gmail.com>
2024-06-10 15:48:21 +0530

    fixed ui

--- apps/frontend/src/pages/PrivateRoutes.tsx
+++
@@ -1,9 +0,0 @@
-import { useUser } from "@repo/store/hooks";
-import { Outlet, Navigate } from "react-router-dom";
-
-function PrivateRoute() {
-  const token = useUser();
-  return token ? <Outlet /> : <Navigate to="/login" />;
-}
-
-export default PrivateRoute;
